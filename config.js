fa01f26ed49e05867264c58c24d2f3a98bfb0211
MichaelJahns <jahnsmic001@gmail.com>
2020-06-17 17:36:15 -0700

    Prometheus can now be launched throw nodemon and vscode

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const firebaseConfig = {
-   apiKey: process.env.FIREBASE_API_KEY,
-   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-   databaseURL: process.env.FIREBASE_DB_URL,
-   projectId: process.env.FIREBASE_PROJECT_ID,
-   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.FIREBASE_SENDER_ID,
-   appId: process.env.FIREBASE_APP_ID
+   apiKey: process.env.API_KEY,
+   authDomain: process.env.AUTH_DOMAIN,
+   databaseURL: process.env.DATABASE_URL,
+   projectId: process.env.PROJECT_ID,
+   storageBucket: process.env.STORAGE_BUCKET,
+   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+   appId: process.env.APP_ID
 };
 
 exports.firebaseConfig = firebaseConfig;
